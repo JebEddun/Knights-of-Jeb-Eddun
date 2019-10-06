ff0e3d07e81b752769b5776f2fa284178d65e25b
Nikita Makeev <whale2.box@gmail.com>
2019-10-07 00:22:18 +0200

    Add RnD to the mix

--- KnightsOfJebEddun/KnightsBehaviour.cs
+++ KnightsOfJebEddun/KnightsBehaviour.cs
@@ -12,6 +12,13 @@ namespace KnightsOfJebEddun
                     ScenarioUpgradeableFacilities.protoUpgradeables[
                         SpaceCenterFacility.MissionControl.ToString()].facilityRefs[0].SetLevel(2);
                 }
+                
+                currentLvl = ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.ResearchAndDevelopment);
+                if (currentLvl < 2)
+                {
+                    ScenarioUpgradeableFacilities.protoUpgradeables[
+                        SpaceCenterFacility.ResearchAndDevelopment.ToString()].facilityRefs[0].SetLevel(2);
+                }
             }
     }
 }
