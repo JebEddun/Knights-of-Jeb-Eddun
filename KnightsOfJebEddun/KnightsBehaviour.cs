b4cae7d680ce363c8a58934ad5b520de25757b39
Nikita Makeev <whale2.box@gmail.com>
2019-10-07 02:57:49 +0200

    Change facility names

--- KnightsOfJebEddun/KnightsBehaviour.cs
+++ KnightsOfJebEddun/KnightsBehaviour.cs
@@ -6,27 +6,30 @@ namespace KnightsOfJebEddun
 {
     public class KnightsBehaviour : ContractBehaviour
     {
-            protected override void OnAccepted()
+        private string MissionControl;
+        private string RnD;
+
+        public KnightsBehaviour()
+        {
+            MissionControl = "SpaceCenter/" + SpaceCenterFacility.MissionControl;
+            RnD = "SpaceCenter/" + SpaceCenterFacility.ResearchAndDevelopment;
+            
+            Debug.Log($"Knights: MissionControl = {ScenarioUpgradeableFacilities.GetFacilityName(SpaceCenterFacility.MissionControl)}");
+
+        }
+
+        protected override void OnAccepted()
             {
                 float currentLvl = ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.MissionControl);
                 if (currentLvl < 2)
                 {
-                    Debug.Log($"Knights: MissionControl = {SpaceCenterFacility.MissionControl} ({SpaceCenterFacility.MissionControl.ToString()})");
-                    Debug.Log($"Knights: RnD = {SpaceCenterFacility.ResearchAndDevelopment} ({SpaceCenterFacility.ResearchAndDevelopment.ToString()})");
-
-                    foreach (String k in ScenarioUpgradeableFacilities.protoUpgradeables.Keys)
-                    {
-                        Debug.Log($"Knights: k = {k}");
-                    }
-                    ScenarioUpgradeableFacilities.protoUpgradeables[
-                        SpaceCenterFacility.MissionControl.ToString()].facilityRefs[0].SetLevel(2);
+                    ScenarioUpgradeableFacilities.protoUpgradeables[MissionControl].facilityRefs[0].SetLevel(2);
                 }
                 
                 currentLvl = ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.ResearchAndDevelopment);
                 if (currentLvl < 2)
                 {
-                    ScenarioUpgradeableFacilities.protoUpgradeables[
-                        SpaceCenterFacility.ResearchAndDevelopment.ToString()].facilityRefs[0].SetLevel(2);
+                    ScenarioUpgradeableFacilities.protoUpgradeables[RnD].facilityRefs[0].SetLevel(2);
                 }
             }
     }
